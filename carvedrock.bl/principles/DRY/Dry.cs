65e04aad3c9376c1ad817992628c82ab631f2c77
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-03-19 07:46:23 +0800

    DRY - DON'T REPEAT YOURSELF WET - WRITE EVERYTHING TWICE

---
+++ carvedrock.bl/principles/DRY/Dry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carvedrock.bl.principles.DRY
+{
+    internal class DRY
+    {
+        public DRY()
+        {
+            BackPack backPack = new BackPack("Tor 85 Litre Rucksack", 79.99, 85, 200, true);
+            Shoes shoes = new Shoes("Salomon X Ultra", 119, 10, "black", true);
+
+            backPack.Price = PriceWithDiscount(backPack.Price);
+            shoes.Price = PriceWithDiscount(shoes.Price);
+        }
+        public static double PriceWithDiscount(double price)
+        {
+            if ((price > 100) && (price < 400))
+            {
+                price -= price * 0.30;
+            }
+            else if (price >= 400)
+            {
+                price -= price * 0.60;
+            }
+            return price;
+        }
+    }
+}
--- carvedrock.bl/principles/DRY/Shoes.cs
+++ carvedrock.bl/principles/DRY/Shoes.cs
@@ -17,17 +17,6 @@ namespace carvedrock.bl.principles.DRY
 			IsWaterproof = isWaterproof;
 		}
 
-		public static double PriceWithDiscount(double price)
-		{
-			if ((price > 100) && (price < 400))
-			{
-				price -= price * 0.30;
-			}
-			else if (price >= 400)
-			{
-				price -= price * 0.60;
-			}
-			return price;
-		}
+		
 	}
 }

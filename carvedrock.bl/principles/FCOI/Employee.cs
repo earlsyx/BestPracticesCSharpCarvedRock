6cffa74824a79e85f62dac426fe2b19fb1361807
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-03-21 08:05:48 +0800

    FCOI - Favor Composition Over Inheritance

--- carvedrock.bl/principles/FCOI/Employee.cs
+++ carvedrock.bl/principles/FCOI/Employee.cs
@@ -1,7 +1,14 @@
 ﻿namespace carvedrock.bl.principles.FCOI
 {
-    public class Employee : Person
+    public class Employee
     {
         public int salary;
+        public Person person { get; set; }
+        public Employee(Person p, int s)
+        {
+            salary = s;
+            person = p;
+        }
+
     }
 }
